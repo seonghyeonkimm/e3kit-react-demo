3fa2f897d55ecd0a58828d21573429762c1be07c
seonghyeon <impressor7@gmail.com>
2020-02-15 20:43:19 +0900

    add default ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
+  const logs = [{ title: 'test1 init', result: '...' }, { title: 'test2 init', result: '...'}]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box py={2}>
+        <Box display="flex" mb={3}>
+          <Box mr={2}>
+            <Button variant="contained" color="primary">START</Button>
+          </Box>
+          <Button variant="contained">RESET</Button>
+        </Box>
+        <Box>
+          <Typography variant="h4">Test E3Kit Methods</Typography>
+          <Box mt={2}>
+            <LogContainer logs={logs} />
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
+}
+
+const LogContainer: React.FC<{ logs: { title: string; result: string; }[]}> = ({ logs }) => {
+  return (
+    <>
+      {
+        logs.map((log ,index) => {
+          return (
+            <Box mb={2} key={index.toString()}>
+              <Typography variant="h6">{log.title}</Typography>
+              <Typography variant="body1">{log.result}</Typography>
+            </Box>
+          );
+        })
+      }
+    </>
+  )
 }
 
 export default App;
